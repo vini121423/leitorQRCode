d468832b2481e9e6c787816b993663db086b0b7b
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-10-07 12:11:00 -0300

    Criação do leitor de QrCode

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+	public leitorQRCode: any;
+	public content: HTMLElement;
+	public imgLogo: HTMLElement;
+	
+	public leitura: string;
 
-  constructor() {}
+  constructor(private qrScanner: QRScanner,
+              public alertController: AlertController) {}
+   
+   public lerQrCode(){
+	   
+    this.qrScanner.prepare()
+    .then((status: QRScannerStatus) => {
+     if (status.authorized) {
+		 this.content = document.getElementByTagName('ion-content')[0] as HTMLElement;
+		 this.imgLogo = document.getElementById('logo') as HTMLElement;
+		 this.content.style.opacity = "0";
+		 this.imgLogo.style.opacity = "0";
+		 
+		 this.qrScanner.show(); // Show camera preview
+		 
+       this.leitorQRCode = this.qrScanner.scan().subscribe((text: string) => {
+      
+	    this.leitura = (text['result']) ? text['result'] : text;
+ 
+         this.content.style.opacity = "1";
+		 this.imgLogo.style.opacity = "1";
+		 
+		 
+         this.qrScanner.hide();
+         this.leitorQRCode.unsubscribe(); // Stop Scanning
+		 
+		 this.presentAlert('LEITURA:,' this.leitura);
+       });
 
+     } else if (status.denied) {
+		 
+     } else {
+		 
+     }
+  })
+  .catch((e: any) => console.log('Erro na aplicação', e));
+   }
+   
+    async presentAlert(title:string,msg:string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
